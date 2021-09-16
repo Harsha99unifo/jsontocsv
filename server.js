d25efb200ab673ff7214ef704dd93dd95f1a2d48
Harsha99unifo <89625573+Harsha99unifo@users.noreply.github.com>
2021-09-16 11:00:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 'use strict';
 const Hapi = require('@hapi/hapi');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+/*const createCsvWriter = require('csv-writer').createObjectCsvWriter;*/
 const writeCSV = require('write-csv');
 let format = require('date-format');
 const date=format('dd-MM-yyyy',new Date());
+const xlsx = require('xlsx');
 console.log(date);
 //read jsonfile
 var readJsonStream = require('read-json-stream').default;
 //onst jsondata =  readjson('./data.json');
 console.log(readJsonStream);
-const csvWriter=createCsvWriter({
+/*const csvWriter=createCsvWriter({
       path: "T:/jsontocsv/geek_data.csv",
       header: [
          {id:"id"},
           {id: "name"},
           {id: "phoneNumber"}
       ]
-});
+});*/
 
 
 const init = async () => {
@@ -31,21 +32,21 @@ const init = async () => {
             path:'/',
             handler:  (request, h)=>
             {
-              readJsonStream('data.json').done((err,data)=>{
+              readJsonStream('data.json').done((err,datagot)=>{
                 if(err){
                    console.log(err);
                 }
                 else
                 { 
                     //console.log(date);              
-                    //const writeCsv = require('write-csv');
-                    //writeCSV('gree_data.csv', data);   
-                    csvWriter.writeRecords(data).then(()=> console.log('data uploaded into csv sucessfully'));
-                   /*const newBook = xlsx.utils.book_new();
-                   const newSheet = xlsx.utils.json_to_sheet(data);
+                    /**const writeCsv = require('write-csv');
+                    writeCSV('gree_data.csv', data);   
+                    //csvWriter.writeRecords(data).then(()=> console.log('data uploaded into csv sucessfully'));
+                   */const newBook = xlsx.utils.book_new();
+                   const newSheet = xlsx.utils.json_to_sheet(datagot);
                    xlsx.utils.book_append_sheet(newBook,newSheet,date);
                    xlsx.writeFile(newBook,"DIALY_DICE_REPORT.xlsx");
-                   */
+                   
                       
                 }
                 
